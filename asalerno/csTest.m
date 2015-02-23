0a63f9e638d99f5481199a79a00e0026e33e771f
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2015-02-23 11:04:39 -0500

    Changes made in order to make the code more similar to Lustig's. If we wish to add the directionality afterwards, I will have to tack on a 'filter building' that should be relatively trivial to do -- tack it on using extra code since sampFac and dir are still read into the code

--- asalerno/csTest.m
+++ asalerno/csTest.m
@@ -1,10 +1,10 @@
 file = {'/projects/muisjes/asalerno/CS/data/RealImgRaw.10.18.mnc' ...
     '/projects/muisjes/asalerno/CS/data/ImagImgRaw.10.18.mnc'};
 sty = 'circ'; % Fully sampled region style
-sampFac = 0.5; % Undersampling factor
-loc = 100; % slice that we want to get
-
-[data,k,pdf] = testMap(file,sty,sampFac,loc);
+sampFac = 0.75; % Undersampling factor
+sl = 100; % slice that we want to get
+loc = 1;
+im = testMap(file,sty,sl,loc);
 
 N = size(data);		% image Size
 DN = N;         	% data Size
@@ -14,9 +14,20 @@ TVWeight = 0.01; 	% Weight for TV penalty
 xfmWeight = 0.01;	% Weight for Transform L1 penalty
 Itnlim = 8;         % Number of iterations
 
+% generate variable density random sampling
+pdf = genPDF(DN,P,pctg, 2 ,0.1,0);	% generates the sampling PDF - L2
+% In this case, there is a clear dropoff in the density of the sampling as
+% one gets away from the centre - optially it appears as if there is an
+% isotropic fall off
+k = genSampling(pdf,10,60);		% generates a sampling pattern
+
 % Generate the required operators
 FT = p2DFT(k,N,1,2);
+data = FT*im;
 XFM = Wavelet('Daubechies',6,4);	% Wavelet - gives a 1x1 matrix, of value 1
+%XFM = TIDCT(8,4);			% DCT
+%XFM = 1;				% Identity transform
+
 
 % initialize Parameters for reconstruction
 param = init;
@@ -40,16 +51,24 @@ tic
 for n=1:Itnlim
     res = fnlCg(res,param);
     im_res = XFM'*res;
-    figure(100), imshow(abs(im_res),[]), drawnow
+    if n==Itnlim; imshow(abs(im_res),[]); end
 end
 toc
 
-mask_lr = genLRSampling_pctg(DN,pctg,1,0); % Creates a mask just to view a low res image
-im_lr = ifft2c(zpad(data.*mask_lr,N(1),N(2)));
-
-im_full = ifft2c(zpad(fft2c(data),N(1),N(2))); % Full image
-figure, imshow(abs(cat(2,im_full,im_lr,im_dc,im_res)),[]);
-title('original             low-res              zf-w/dc              TV');
-
-figure, plot(1:N(1), abs(im_full(end/2,:)),1:N(1), abs(im_lr(end/2,:)), 1:N(2), abs(im_dc(end/2,:)), 1:N(2), abs(im_res(end/2,:)),'LineWidth',2);
-legend('original', 'LR', 'zf-w/dc', 'TV');
+figure;
+subplot(131);
+imshow(abs(im_dc),[]);
+subplot(132);
+imshow(abs(im_res),[]);
+subplot(133);
+imshow(abs(im_res)-abs(im_dc),[]);
+suptitle('Original undersampled           CS            Residual')
+% mask_lr = genLRSampling_pctg(DN,pctg,1,0); % Creates a mask just to view a low res image
+% im_lr = ifft2c(zpad(data.*mask_lr,N(1),N(2)));
+% 
+% im_full = ifft2c(zpad(fft2c(data),N(1),N(2))); % Full image
+% figure, imshow(abs(cat(2,im_full,im_lr,im_dc,im_res)),[]);
+% title('original             low-res              zf-w/dc              TV');
+% 
+% figure, plot(1:N(1), abs(im_full(end/2,:)),1:N(1), abs(im_lr(end/2,:)), 1:N(2), abs(im_dc(end/2,:)), 1:N(2), abs(im_res(end/2,:)),'LineWidth',2);
+% legend('original', 'LR', 'zf-w/dc', 'TV');
--- asalerno/testMap.m
+++ asalerno/testMap.m
@@ -1,4 +1,4 @@
-function [img,filTot,pdf] = testMap(file,sty,sampFac,sl,loc,ksp,dir)
+function img = testMap(file,sty,sl,loc,dir)
 %
 % testMap.m                         02/18/15
 %
@@ -8,9 +8,8 @@ function [img,filTot,pdf] = testMap(file,sty,sampFac,sl,loc,ksp,dir)
 %
 % Make sure that we have all the required information for the strcmp
 
-if nargin < 5; loc = 1; end
-if nargin < 6; ksp = 0; end
-if nargin < 7; dir = []; end
+if nargin < 4; loc = 1; end
+if nargin < 5; dir = []; end
 
 if strcmp(sty,'par') || strcmp(sty,'per')
     if isempty(dir)
@@ -59,117 +58,118 @@ if isa(file,'cell')
         dataUse = double(dataUse);
     end
     
+    img = fftshift(fft2(dataUse));
     
-    % Build the basis of the filter
-    if strcmp(sty,'par') || strcmp(sty,'per')
-        slp = dir(2)/dir(1);
-        fil = linefilt(dataUse,slp,sampFac/2);
-    elseif strcmp(sty,'circ')
-        fil = circfilt(dataUse,sampFac/2);
-    elseif strcmp(sty,'lores') || strcmp(sty,'sq')
-        fil = sqfilt(dataUse,sampFac/2);
-    end
-    
-    
-    sz = size(fil);
-    % Add the 1/r component
-    alim = 1/sqrt(2);
-    [x,y] = meshgrid(linspace(-alim,alim,sz(1)),linspace(-alim,alim,sz(2)));
-    r = sqrt(x.^2+y.^2);
-    fil2 = (ones(sz)-fil).*rand(sz(1),sz(2)).*(1-r/max(r(:)));
-    filTot = fil + fil2; % Build the total filter PDF
-    pdf = (1-r).*(ones(sz)-fil)+fil;
-    for i = 1:sz(1)
-        for j = 1:sz(2)
-            if filTot(i,j) < (0.5*sampFac)
-                filTot(i,j) = 0;
-            else
-                filTot(i,j) = 1;
-            end
-        end
-    end
-    % We know that if two files are given, one is the real, the other
-    % is the imaginary
-    img = dataUse.*filTot;
+%     % Build the basis of the filter
+%     if strcmp(sty,'par') || strcmp(sty,'per')
+%         slp = dir(2)/dir(1);
+%         fil = linefilt(dataUse,slp,sampFac/2);
+%     elseif strcmp(sty,'circ')
+%         fil = circfilt(dataUse,sampFac/2);
+%     elseif strcmp(sty,'lores') || strcmp(sty,'sq')
+%         fil = sqfilt(dataUse,sampFac/2);
+%     end
+%     
+%     
+%     sz = size(fil);
+%     % Add the 1/r component
+%     alim = 1/sqrt(2);
+%     [x,y] = meshgrid(linspace(-alim,alim,sz(1)),linspace(-alim,alim,sz(2)));
+%     r = sqrt(x.^2+y.^2);
+%     fil2 = (ones(sz)-fil).*rand(sz(1),sz(2)).*(1-r/max(r(:)));
+%     filTot = fil + fil2; % Build the total filter PDF
+%     pdf = (1-r).*(ones(sz)-fil)+fil;
+%     for i = 1:sz(1)
+%         for j = 1:sz(2)
+%             if filTot(i,j) < (0.75*sampFac)
+%                 filTot(i,j) = 1;
+%             else
+%                 filTot(i,j) = 0;
+%             end
+%         end
+%     end
+%     % We know that if two files are given, one is the real, the other
+%     % is the imaginary
+%     img = dataUse.*filTot;
     
     
 else
-    % Read in the data
-    data = mincread(file,'image');
-    
-    % Process it for powers of two
-    dims = size(data);
-    
-    dims2 = 2.^(nextpow2(dims)-1);
-    
-    for i = 1:3
-        if dims(i) == 2*dims2(i)
-            dims2(i) = dims(i);
-        end
-        if mod(dims(i),2) ~= 0
-            dims(i) = dims(i)+1;
-            if i == 1
-                data = data(1:end-1,:,:);
-            elseif i == 2
-                data = data(:,1:end-1,:);
-            elseif i == 3
-                data = data(:,1:end-1,:);
-            end
-        end
-    end
-    
-    res = ceil((dims - dims2)/2); % The residual that we need to see on either side in order to compress the data to 2^n
-    
-    dataRes = data(res(1)+1:end-res(1),res(2)+1:end-res(2),res(3)+1:end-res(3));
-    if loc == 1
-        dataUse = squeeze(dataRes(round(sl/dims(1)*dims2(1)),:,:));
-    elseif loc == 2
-        dataUse = squeeze(dataRes(:,round(sl/dims(2)*dims2(2)),:));
-    elseif loc == 3
-        dataUse = squeeze(dataRes(:,:,round(sl/dims(3)*dims2(3))));
-    end
-    if isa(dataUse,'uint16')
-        dataUse = double(dataUse);
-    end
-    
-    
-    % Build the basis of the filter
-    if strcmp(sty,'par') || strcmp(sty,'per')
-        slp = dir(2)/dir(1);
-        fil = linefilt(dataUse,slp,sampFac/2);
-    elseif strcmp(sty,'circ')
-        fil = circfilt(dataUse,sampFac/2);
-    elseif strcmp(sty,'lores') || strcmp(sty,'sq')
-        fil = sqfilt(dataUse,sampFac/2);
-    end
-    
-    sz = size(fil);
-    % Add the 1/r component
-    alim = 1/sqrt(2);
-    [x,y] = meshgrid(linspace(-alim,alim,sz(1)),linspace(-alim,alim,sz(2)));
-    r = sqrt(x.^2+y.^2);
-    fil2 = (ones(sz)-fil).*rand(sz(1),sz(2)).*(1-r/max(r(:)));
-    filTot = fil + fil2; % Build the total filter PDF
-    for i = 1:sz(1)
-        for j = 1:sz(2)
-            if filTot(i,j) < (0.5*sampFac)
-                filTot(i,j) = 0;
-            else
-                filTot(i,j) = 1;
-            end
-        end
-    end
-    
-    if ksp == 0
-        % Normalize to be safe
-        imshow(dataUse,[]);
-        dataUse = dataUse./max(dataUse(:));
-        dataUse = fftshift(fft2(dataUse));
-    end
-    img = dataUse.*filTot;
-    
-    if 1 == 1
-        figure; imshow(abs(dataUse),[]);
-        figure; imshow(filTot,[]);
-    end
+%     % Read in the data
+%     data = mincread(file,'image');
+%     
+%     % Process it for powers of two
+%     dims = size(data);
+%     
+%     dims2 = 2.^(nextpow2(dims)-1);
+%     
+%     for i = 1:3
+%         if dims(i) == 2*dims2(i)
+%             dims2(i) = dims(i);
+%         end
+%         if mod(dims(i),2) ~= 0
+%             dims(i) = dims(i)+1;
+%             if i == 1
+%                 data = data(1:end-1,:,:);
+%             elseif i == 2
+%                 data = data(:,1:end-1,:);
+%             elseif i == 3
+%                 data = data(:,1:end-1,:);
+%             end
+%         end
+%     end
+%     
+%     res = ceil((dims - dims2)/2); % The residual that we need to see on either side in order to compress the data to 2^n
+%     
+%     dataRes = data(res(1)+1:end-res(1),res(2)+1:end-res(2),res(3)+1:end-res(3));
+%     if loc == 1
+%         dataUse = squeeze(dataRes(round(sl/dims(1)*dims2(1)),:,:));
+%     elseif loc == 2
+%         dataUse = squeeze(dataRes(:,round(sl/dims(2)*dims2(2)),:));
+%     elseif loc == 3
+%         dataUse = squeeze(dataRes(:,:,round(sl/dims(3)*dims2(3))));
+%     end
+%     if isa(dataUse,'uint16')
+%         dataUse = double(dataUse);
+%     end
+%     
+%     
+%     % Build the basis of the filter
+%     if strcmp(sty,'par') || strcmp(sty,'per')
+%         slp = dir(2)/dir(1);
+%         fil = linefilt(dataUse,slp,sampFac/2);
+%     elseif strcmp(sty,'circ')
+%         fil = circfilt(dataUse,sampFac/2);
+%     elseif strcmp(sty,'lores') || strcmp(sty,'sq')
+%         fil = sqfilt(dataUse,sampFac/2);
+%     end
+%     
+%     sz = size(fil);
+%     % Add the 1/r component
+%     alim = 1/sqrt(2);
+%     [x,y] = meshgrid(linspace(-alim,alim,sz(1)),linspace(-alim,alim,sz(2)));
+%     r = sqrt(x.^2+y.^2);
+%     fil2 = (ones(sz)-fil).*rand(sz(1),sz(2)).*(1-r/max(r(:)));
+%     filTot = fil + fil2; % Build the total filter PDF
+%     for i = 1:sz(1)
+%         for j = 1:sz(2)
+%             if filTot(i,j) < (0.75*sampFac)
+%                 filTot(i,j) = 0;
+%             else
+%                 filTot(i,j) = 1;
+%             end
+%         end
+%     end
+%     
+%     if ksp == 0
+%         % Normalize to be safe
+%         imshow(dataUse,[]);
+%         dataUse = dataUse./max(dataUse(:));
+%         dataUse = fftshift(fft2(dataUse));
+%     end
+%     img = dataUse.*filTot;
+%     
+%     if 1 == 1
+%         figure; imshow(abs(dataUse),[]);
+%         figure; imshow(filTot,[]);
+%     end
 end
